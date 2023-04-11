bba8d0c37318d97b8bec846a42f4e63921fa5426
Ghassen43 <ghassenamara43@gmail.com>
2023-04-11 23:15:13 +0100

    homePage

---
+++ src/components/global/home/HomeDesc.tsx
@@ -0,0 +1,17 @@
+import "../../../styles/components/home/HomeDesc.scss";
+import events from "../../../assets/events.jpg";
+export default function HomeDesc() {
+    return (
+        <div className="Desc">
+            <div  >
+                
+            <h1 className="Desc-title">Let us take the stress out of event planning <br /> so that you can enjoy your special day to the fullest.</h1>
+           <h3 className="cc"> We value transparency and strive to provide clear and accurate information to our clients</h3>
+            </div>
+            <div className="Desc-image">
+                <img src={events} alt="Desc" />
+            </div>
+               
+        </div>
+    );
+}
